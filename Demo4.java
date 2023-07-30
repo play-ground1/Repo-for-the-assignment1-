fa73d3598095fe9435f69f0c7af56da419261e18
Sanduni Haththotuwa <sandunihaththotuwa@gmail.com>
2023-07-30 15:23:10 +0530

    Complete Task 5

--- Demo4.java
+++ Demo4.java
@@ -1,16 +1,16 @@
 public class Demo4 {
     public static void main(String[] args) {
         
-        String COLOR1 = "\033[31m";
-        String COLOR2 = "\033[32m";
-        String COLOR3 = "\033[33m";
-        String COLOR4 = "\033[34m";
-        String COLOR5 = "\033[35m";
-        String COLOR6 = "\033[36m";
-        String RESET = "\033[0m";
+        String color1 = "\033[31m";
+        String color2 = "\033[32m";
+        String color3 = "\033[33m";
+        String color4 = "\033[34m";
+        String color5 = "\033[35m";
+        String color6 = "\033[36m";
+        String reset = "\033[0m";
 
 
-        System.out.printf("%s%d%s/%s%d%s/%s%d%s %s%d%s:%s%d%s:%s%d%s \n",COLOR1,12,RESET,COLOR2,28,RESET,COLOR3,2023,RESET,COLOR4,23,RESET,COLOR5,59,RESET,COLOR6,59,RESET);
+        System.out.printf("%s%d%s/%s%d%s/%s%d%s %s%d%s:%s%d%s:%s%d%s \n",color1,12,reset,color2,28,reset,color3,2023,reset,color4,23,reset,color5,59,reset,color6,59,reset);
 
     }
     
